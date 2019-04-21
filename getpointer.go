516125b1e0a63702728ef6f71b4e50cf378bfb75
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-04-21 20:02:57 +0200

    add code for getting coordinates

--- getpointer.go
+++ getpointer.go
@@ -1,1 +1,31 @@
+package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-vgo/robotgo"
+)
+
+func get() {
+	time.Sleep(2 * time.Second)
+	x, y := robotgo.GetMousePos()
+	fmt.Println("pos:", x, y)
+}
+
+func main() {
+	count := 0
+	max := 20
+
+	for count < max {
+		// delay with 2 seconds
+		time.Sleep(2 * time.Second)
+
+		// get the mouse coordinates
+		get()
+
+		// increment counter
+		count++
+	}
+	fmt.Println("exiting..")
+}
