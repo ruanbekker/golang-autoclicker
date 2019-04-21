9143170a7a24c4b3812f9461db8a1f1dbc76e06c
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-04-21 20:03:41 +0200

    add code for auto clicking

--- autoclick.go
+++ autoclick.go
@@ -1,1 +1,43 @@
+package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-vgo/robotgo"
+)
+
+func moveAndClickStepOne() {
+	fmt.Println("Moving Mouse")
+	robotgo.MoveMouseSmooth(166, 775, 1.0, 7.0)
+	fmt.Println("Clicking")
+	robotgo.Click()
+}
+
+func moveAndClickStepTwo() {
+	fmt.Println("Moving Mouse")
+	robotgo.MoveMouseSmooth(268, 112, 1.0, 7.0)
+	fmt.Println("Clicking")
+	robotgo.Click()
+}
+
+func delay() {
+	fmt.Println("Delaying with 2 Seconds")
+	time.Sleep(2 * time.Second)
+}
+
+func main() {
+	count := 0
+	iterations := 4
+
+	for count <= iterations {
+		fmt.Println("Starting iteration")
+		moveAndClickStepOne()
+		delay()
+		moveAndClickStepTwo()
+		delay()
+		fmt.Println("finising iteration")
+		count++
+	}
+	fmt.Println("finished all iterations")
+}
